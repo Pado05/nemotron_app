57ce00797038e393b08a4453a8c4ff39f5ca6016
Pado05 <alepado05@gmail.com>
2026-05-13 16:17:27 +0200

    prova 3

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 import streamlit as st
-import requests
 
 
 load_dotenv(override=True)
@@ -13,14 +12,19 @@ MODEL = "nvidia/nemotron-3-nano-omni-30b-a3b-reasoning:free"
 
 client = OpenAI(base_url=BASE_URL, api_key=API_KEY)
 
-richiesta = st.input_text("Inserisci la tua richiesta")
+
+richiesta = st.text_area("Scrivi la tua richiesta a Nemotron:")
 
 completion = client.chat.completions.create(
-                                            model=MODEL,
-                                            messages=[
-                                                {"role": "system", "content": "Sei un assistente per lo studio. Cortesemente aiutami con i compiti"},
-                                                {"role": "user", "content": richiesta}
-                                            ]
-                                            )
-st.write(completion.choices[0].message.content)
+    model=MODEL,
+    messages=[
+        {"role": "system", "content": "Sei un assistente per lo studio. Sei cortese e aiuti gli utenti con i loro compiti."},
+        {"role": "user", "content": richiesta}
+    ]
+)
+risposta = completion.choices[0].message.content
+
+
+st.write(risposta)
+                
 
