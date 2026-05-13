ca5838be52e637894e6a6fa2bc77fc87c6711397
Pado05 <alepado05@gmail.com>
2026-05-13 15:52:20 +0200

    prova 1

---
+++ app.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+import streamlit as st
+import requests
+
+
+load_dotenv(override=True)
+
+API_KEY = os.getenv('OPENAI_API_KEY')
+BASE_URL = "https://openrouter.ai/api/v1"
+MODEL = "nvidia/nemotron-3-nano-omni-30b-a3b-reasoning:free"
+
+client = OpenAI(base_url=BASE_URL, api_key=API_KEY)
+
+completion = client.chat.completions.create(
+                                            extra_body={},
+                                            model=MODEL,
+                                            messages=[
+                                                {
+                                                "role": "user",
+                                                "content": "What is the meaning of life?"
+                                                }
+                                            ]
+                                            )
+st.chat_input(completion.choices[0].message.content)
+
